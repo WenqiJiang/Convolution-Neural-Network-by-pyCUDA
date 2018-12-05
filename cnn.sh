f3a95ba14de598c0e6b0c2823730f89d41ffae6e
WenqiJiang <42897680+WenqiJiang@users.noreply.github.com>
2018-12-04 22:52:28 -0500

    delete useless comments

--- cnn.sh
+++ cnn.sh
@@ -1,6 +1,6 @@
 rm sl*
 sbatch --gres=gpu:1 --time=30 --wrap="python run.py"
-#sbatch --gres=gpu:1 --time=3 --wrap="python opencl.py"
+
 while [ ! -e sl* ] # while slurm.out does not exist
 do
     sleep 2
